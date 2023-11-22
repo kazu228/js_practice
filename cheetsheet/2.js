9df2a1f1b09080763100d80f1b5604503f58ea83
Kazu <kskkm0228@gmail.com>
2023-11-23 01:40:44 +0900

    console.log

---
+++ cheetsheet/2.js
@@ -0,0 +1,29 @@
+let greeting = "Hello. JavaScript";
+console.log(greeting);
+
+greeting = "Hello, World!";
+console.log(greeting);
+
+let obj = {
+    name : "Alice",
+    age : 20
+};
+
+// console.log(obj.name);
+// console.log(obj.age);
+
+
+
+let myVar;
+console.log(myVar);
+
+
+
+
+
+
+
+
+
+
+
