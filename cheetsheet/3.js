29a75f6fbbcf9ac8840a3fd82f5f5ec59d9111ad
Kazu <kskkm0228@gmail.com>
2023-11-24 01:50:05 +0900

    undefinedの確認

--- cheetsheet/3.js
+++ cheetsheet/3.js
@@ -6,3 +6,42 @@ let lang = "Hello, JavaScript";
 
 console.log(lang);
 
+let obj = {
+    name : "Alice",
+    age : 20
+};
+
+// console.log(obj.name);
+// console.log(obj.age);
+
+let nullValue = null;
+console.log(nullValue);
+
+let undefinedValue;
+console.log(undefinedValue);
+
+const myObj = {
+    a : 1
+};
+console.log(myObj.b);
+
+function doNothing() {}
+console.log(doNothing());
+
+function doSomething() {
+    return '';
+}
+
+
+console.log("doSomething");
+console.log(doSomething());
+
+
+
+
+
+
+
+
+
+
