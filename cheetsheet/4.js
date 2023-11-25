90583e2389e0c234c6355e97d9d0d69b16caff77
Kazu <kskkm0228@gmail.com>
2023-11-25 23:17:26 +0900

    nullとundefined

--- cheetsheet/4.js
+++ cheetsheet/4.js
@@ -5,3 +5,9 @@ let obj = { name : "Alice", age: 20};
 console.log(obj.name);
 console.log(obj.age);
 
+let nullValue = null;
+console.log(nullValue);
+
+let undefinedValue;
+console.log(undefinedValue);
+
