139493b92c0410df3b450122478633006b6dbcf7
Kazu <kskkm0228@gmail.com>
2023-11-30 06:15:23 +0900

    変数と演算

---
+++ cheetsheet/5.js
@@ -0,0 +1,19 @@
+let greeting = 'Hello World';
+console.log(greeting);
+
+let msg = 'Hello Node.js';
+console.log(msg);
+
+let a = 10;
+let b = 20;
+let c = a + b;
+console.log(c);
+
+c = a * b;
+console.log(c);
+
+c = a / b;
+console.log(c);
+
+
+
