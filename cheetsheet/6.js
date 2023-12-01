7b9039be8017160bdf6b68c673f1d05c15a43553
Kazu <kskkm0228@gmail.com>
2023-12-02 02:54:41 +0900

    条件分岐

--- cheetsheet/6.js
+++ cheetsheet/6.js
@@ -24,5 +24,17 @@ let name = 'John';
 console.log(`Hello ${ name } !!`);
 
 
+let price = 100;
+let total = price * 1.10;
+console.log(`合計金額 ${ total } 円`);
+
+if (total >= 100) {
+    console.log('これは、高い。');
+} else {
+    console.log('これは、安い。');
+}
+
+
+
 
 
