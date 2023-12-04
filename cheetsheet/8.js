050b6ab255f949e83848901c7ebef9e447eded77
Kazu <kskkm0228@gmail.com>
2023-12-04 23:10:17 +0900

    オブジェクトの定数の注意点

--- cheetsheet/8.js
+++ cheetsheet/8.js
@@ -10,5 +10,25 @@ console.log(tax);
 
 // console.log(tax);
 
+let obj = { a: 'a'};
+
+console.log(obj.a);
+
+const obj2 = { abc : 'ABC'};
+
+console.log(obj2.abc);
+// obj2 = { def : 'DEF'};   // NG
+
+
+obj2.abc = 'abc';
+console.log(obj2.abc);    // OK
+
+// undefined
+console.log(obj.b);
+
+
+
+
+
 
 
