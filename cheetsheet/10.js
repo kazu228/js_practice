bff1f04c498de6301a1d09d294dd32d38efd1825
Kazu <kskkm0228@gmail.com>
2023-12-07 22:17:07 +0900

    undefinedの復習

--- cheetsheet/10.js
+++ cheetsheet/10.js
@@ -2,3 +2,17 @@ let g = 'Hello World';
 
 console.log(g);
 
+let obj = { a : 'a' };
+
+console.log(obj.a);
+
+let undefinedValue;
+
+console.log(undefinedValue);
+
+console.log(obj.b);
+
+function doNothing() {}
+
+console.log(doNothing());
+
