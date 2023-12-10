345ffe3098e6222f23f143b4d880b384f5eed0a5
Kazu <kskkm0228@gmail.com>
2023-12-10 21:16:22 +0900

    undefinedとオブジェクト

--- cheetsheet/12.js
+++ cheetsheet/12.js
@@ -2,3 +2,17 @@ let greeting = 'Hello World';
 
 console.log(greeting);
 
+let obj = { a : 'a' };
+
+console.log(obj.a);
+
+console.log(obj.b);
+
+let unde;
+
+console.log(unde);
+
+function doNothing() {}
+
+console.log(doNothing());
+
