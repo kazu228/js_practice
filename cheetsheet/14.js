6ba25475061e4637f74147ae1a4d883509188ca3
Kazu <kskkm0228@gmail.com>
2023-12-15 23:44:11 +0900

    undefined

--- cheetsheet/14.js
+++ cheetsheet/14.js
@@ -7,4 +7,13 @@ let user = { name : 'kazu', age : 25};
 console.log(user.name);
 console.log(user.age);
 
+let a;
+console.log(a);
+
+console.log(user.address);
+
+function doNothing() {}
+
+console.log(doNothing());
+
 
