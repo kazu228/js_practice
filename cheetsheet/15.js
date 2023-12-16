d75bc17b1597b8514986350bcb8d12b903325141
Kazu <kskkm0228@gmail.com>
2023-12-16 23:38:28 +0900

    スイカとプレーヤーの距離の計算

--- cheetsheet/15.js
+++ cheetsheet/15.js
@@ -17,5 +17,7 @@ let suika_y = Math.floor( Math.random() * (max + 1 - min)) + min;
 function cal_distance(x1, y1, x2, y2) {
    let diff_x = x1 - x2;
    let diff_y = y1 - y2;
-
+   return Math.sqrt(diff_x ** 2 + diff_y ** 2); 
 }
+
+console.log(cal_distance(player_x, player_y, suika_x, suika_y));
