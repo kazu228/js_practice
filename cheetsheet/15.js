6765d9bf486f266a1ce75b02b499a94a3a47abc2
Kazu <kskkm0228@gmail.com>
2023-12-16 22:56:28 +0900

    座標の設定

---
+++ cheetsheet/15.js
@@ -0,0 +1,21 @@
+
+let min = 0;
+let max = 5;
+
+// var a = Math.floor( Math.random() * (max + 1 - min) ) + min ;
+// let player_x = Math.floor()
+
+
+let player_x = Math.floor( Math.random() * (max + 1 - min)) + min;
+
+let player_y = Math.floor( Math.random() * (max + 1 - min)) + min;
+
+let suika_x = Math.floor( Math.random() * (max + 1 - min)) + min;
+
+let suika_y = Math.floor( Math.random() * (max + 1 - min)) + min;
+
+function cal_distance(x1, y1, x2, y2) {
+   let diff_x = x1 - x2;
+   let diff_y = y1 - y2;
+
+}
