2657444e14c0cb3462b07c0d03061df64431d4bf
Kazu <kskkm0228@gmail.com>
2023-12-19 22:54:20 +0900

    距離を求める関数を定義

--- cheetsheet/17.js
+++ cheetsheet/17.js
@@ -3,5 +3,14 @@ let player = { x : 10, y : 30 };
 
 let suika = { x : 20, y : 20 };
 
+function calDistance(x1, y1, x2, y2){
+    let diff_x = x1 - x2;
+    let diff_y = y1 - y2;
 
+    return Math.sqrt(diff_x * diff_x + diff_y * diff_y);
+}
+
+let d = calDistance(player.x, player.y, suika.x, suika.y);
+
+console.log(d);
 
